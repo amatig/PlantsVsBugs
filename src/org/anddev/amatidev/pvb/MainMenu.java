5f43ca01f7345e45ef60a61e699100e7191af951
Giovanni Amati <amatig@gmail.com>
2014-06-11 21:39:50 +0200

    fix

--- src/org/anddev/amatidev/pvb/MainMenu.java
+++ src/org/anddev/amatidev/pvb/MainMenu.java
@@ -5,10 +5,10 @@ import org.amatidev.util.AdEnviroment;
 import org.amatidev.util.AdPrefs;
 import org.anddev.amatidev.pvb.singleton.GameData;
 import org.anddev.andengine.entity.IEntity;
+import org.anddev.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
 import org.anddev.andengine.entity.modifier.LoopEntityModifier;
 import org.anddev.andengine.entity.modifier.ScaleModifier;
 import org.anddev.andengine.entity.modifier.SequenceEntityModifier;
-import org.anddev.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
 import org.anddev.andengine.entity.scene.menu.MenuScene;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.text.Text;
@@ -19,13 +19,11 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 
-import com.openfeint.api.ui.Dashboard;
-
 public class MainMenu extends AdScene {
-	
+
 	private int mIndex;
 	private boolean mContinue;
-	
+
 	@Override
 	public MenuScene createMenu() {
 		return null;
@@ -35,55 +33,50 @@ public class MainMenu extends AdScene {
 	public void createScene() {
 		Sprite back = new Sprite(0, 0, GameData.getInstance().mMainBackground);
 		getChild(BACKGROUND_LAYER).attachChild(back);
-		
+
 		Sprite title = new Sprite(3, 9, GameData.getInstance().mMainTitle);
 		getChild(BACKGROUND_LAYER).attachChild(title);
-		title.registerEntityModifier(
-				new LoopEntityModifier(
-						null, 
-						-1, 
-						null,
-						new SequenceEntityModifier(
-								new ScaleModifier(0.7f, 1f, 1.02f),
-								new ScaleModifier(0.7f, 1.02f, 1f)
-						)
-				)
-		);
-		
+		title.registerEntityModifier(new LoopEntityModifier(null, -1, null,
+				new SequenceEntityModifier(new ScaleModifier(0.7f, 1f, 1.02f),
+						new ScaleModifier(0.7f, 1.02f, 1f))));
+
 		int x = AdEnviroment.getInstance().getScreenWidth() / 2;
-		
+
 		this.mIndex = 159;
-		
-    	Text play = new Text(0, 0, GameData.getInstance().mFontMainMenu, "PLAY");
-    	//play.setColor(1.0f, 1.0f, 0.6f);
-    	play.setPosition(x - play.getWidthScaled() / 2, this.mIndex + 50);
-    	
-    	Text cont = new Text(0, 0, GameData.getInstance().mFontMainMenu, "CONTINUE");
-    	//more.setColor(1.0f, 1.0f, 0.6f);
-    	cont.setPosition(x - cont.getWidthScaled() / 2, this.mIndex + 126);
-    	
-    	Text score = new Text(0, 0, GameData.getInstance().mFontMainMenu, "SCORE");
-    	//score.setColor(1.0f, 1.0f, 0.6f);
-    	score.setPosition(60, this.mIndex + 200);
-    	
-    	Text sep = new Text(0, 0, GameData.getInstance().mFontMainMenu, "/");
-    	//score.setColor(1.0f, 1.0f, 0.6f);
-    	sep.setPosition(250, this.mIndex + 200);
-    	
-    	Text more = new Text(0, 0, GameData.getInstance().mFontMainMenu, "MORE GAME");
-    	//score.setColor(1.0f, 1.0f, 0.6f);
-    	more.setPosition(300, this.mIndex + 200);
-    	
-    	getChild(AdScene.GAME_LAYER).attachChild(play);
-    	getChild(AdScene.GAME_LAYER).attachChild(cont);
-    	getChild(AdScene.GAME_LAYER).attachChild(sep);
-    	getChild(AdScene.GAME_LAYER).attachChild(score);
-    	getChild(AdScene.GAME_LAYER).attachChild(more);
-    	
-    	registerTouchArea(play);
-    	registerTouchArea(cont);
-    	registerTouchArea(score);
-    	registerTouchArea(more);
+
+		Text play = new Text(0, 0, GameData.getInstance().mFontMainMenu, "PLAY");
+		// play.setColor(1.0f, 1.0f, 0.6f);
+		play.setPosition(x - play.getWidthScaled() / 2, this.mIndex + 50);
+
+		Text cont = new Text(0, 0, GameData.getInstance().mFontMainMenu,
+				"CONTINUE");
+		// more.setColor(1.0f, 1.0f, 0.6f);
+		cont.setPosition(x - cont.getWidthScaled() / 2, this.mIndex + 126);
+
+		Text score = new Text(0, 0, GameData.getInstance().mFontMainMenu,
+				"SCORE");
+		// score.setColor(1.0f, 1.0f, 0.6f);
+		score.setPosition(60, this.mIndex + 200);
+
+		Text sep = new Text(0, 0, GameData.getInstance().mFontMainMenu, "/");
+		// score.setColor(1.0f, 1.0f, 0.6f);
+		sep.setPosition(250, this.mIndex + 200);
+
+		Text more = new Text(0, 0, GameData.getInstance().mFontMainMenu,
+				"MORE GAME");
+		// score.setColor(1.0f, 1.0f, 0.6f);
+		more.setPosition(300, this.mIndex + 200);
+
+		getChild(AdScene.GAME_LAYER).attachChild(play);
+		getChild(AdScene.GAME_LAYER).attachChild(cont);
+		getChild(AdScene.GAME_LAYER).attachChild(sep);
+		getChild(AdScene.GAME_LAYER).attachChild(score);
+		getChild(AdScene.GAME_LAYER).attachChild(more);
+
+		registerTouchArea(play);
+		registerTouchArea(cont);
+		registerTouchArea(score);
+		registerTouchArea(more);
 	}
 
 	@Override
@@ -91,9 +84,10 @@ public class MainMenu extends AdScene {
 		GameData.getInstance().mMyLevel.resetScore();
 		GameData.getInstance().mMyScore.resetScore();
 		GameData.getInstance().mMySeed.resetScore();
-		
+
 		if (this.mContinue) {
-			int lev = AdPrefs.getValue(AdEnviroment.getInstance().getContext(), "level", 0);
+			int lev = AdPrefs.getValue(AdEnviroment.getInstance().getContext(),
+					"level", 0);
 			GameData.getInstance().mMyLevel.addScore(lev);
 			AdEnviroment.getInstance().setScene(new MainGame());
 		} else {
@@ -105,24 +99,23 @@ public class MainMenu extends AdScene {
 	public void manageAreaTouch(final ITouchArea pTouchArea) {
 		final Text item = (Text) pTouchArea;
 		item.setColor(1f, 0.7f, 0.7f);
-		
-		item.registerEntityModifier(
-				new SequenceEntityModifier(
-						new IEntityModifierListener() {
-							@Override
-							public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
-								item.setColor(1.0f, 1.0f, 1.0f);
-								MainMenu.this.execute(pTouchArea);
-							}
 
-							@Override
-							public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
-								
-							}
-						},
-						new ScaleModifier(0.1f, 1f, 1.3f),
-						new ScaleModifier(0.1f, 1.3f, 1f)
-		));
+		item.registerEntityModifier(new SequenceEntityModifier(
+				new IEntityModifierListener() {
+					@Override
+					public void onModifierFinished(
+							IModifier<IEntity> pModifier, IEntity pItem) {
+						item.setColor(1.0f, 1.0f, 1.0f);
+						MainMenu.this.execute(pTouchArea);
+					}
+
+					@Override
+					public void onModifierStarted(IModifier<IEntity> pModifier,
+							IEntity pItem) {
+
+					}
+				}, new ScaleModifier(0.1f, 1f, 1.3f), new ScaleModifier(0.1f,
+						1.3f, 1f)));
 	}
 
 	private void execute(ITouchArea pTouchArea) {
@@ -136,40 +129,46 @@ public class MainMenu extends AdScene {
 			AdEnviroment.getInstance().nextScene();
 		} else if ((int) item.getY() == this.mIndex + 200) {
 			if ((int) item.getX() > 100) {
-				try{
-					AdEnviroment.getInstance().getContext().startActivity(new Intent (Intent.ACTION_VIEW, Uri.parse("market://details?id=org.anddev.andengine.braingamelite")));
+				try {
+					AdEnviroment
+							.getInstance()
+							.getContext()
+							.startActivity(
+									new Intent(
+											Intent.ACTION_VIEW,
+											Uri.parse("market://details?id=org.anddev.andengine.braingamelite")));
 				} catch (ActivityNotFoundException e) {
 				}
 			} else {
 				try {
-					Dashboard.open();
+					// Dashboard.open();
 				} catch (Exception e) {
 				}
 			}
 		}
 	}
-	
+
 	@Override
 	public void startScene() {
-		
+
 	}
 
 	@Override
 	public void downSceneTouch(TouchEvent pSceneTouchEvent) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void moveSceneTouch(TouchEvent pSceneTouchEvent) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void upSceneTouch(TouchEvent pSceneTouchEvent) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- src/org/anddev/amatidev/pvb/PlantsVsBugs.java
+++ src/org/anddev/amatidev/pvb/PlantsVsBugs.java
@@ -1,8 +1,5 @@
 package org.anddev.amatidev.pvb;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.amatidev.activity.AdGameActivity;
 import org.amatidev.util.AdEnviroment;
 import org.anddev.amatidev.pvb.singleton.GameData;
@@ -13,13 +10,18 @@ import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.SplashScene;
 
-import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
 
-import com.openfeint.api.OpenFeint;
-import com.openfeint.api.OpenFeintDelegate;
-import com.openfeint.api.OpenFeintSettings;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class PlantsVsBugs extends AdGameActivity {
+	private AdView mAdView;
+	private InterstitialAd interstitial;
 
 	@Override
 	protected int getLayoutID() {
@@ -32,23 +34,67 @@ public class PlantsVsBugs extends AdGameActivity {
 	}
 
 	@Override
-	public void onLoadComplete() {
-		try {
-			Map<String, Object> options = new HashMap<String, Object>();
-			options.put(OpenFeintSettings.SettingCloudStorageCompressionStrategy, OpenFeintSettings.CloudStorageCompressionStrategyDefault);
-			// use the below line to set orientation
-			options.put(OpenFeintSettings.RequestedOrientation, ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-			OpenFeintSettings settings = new OpenFeintSettings(getString(R.string.app_name), getString(R.string.productKey), getString(R.string.productSecret), getString(R.string.clientAppID), options);
-			
-			OpenFeint.initialize(this, settings, new OpenFeintDelegate() { });
-		} catch (Exception e) {
-			
+	protected void onCreate(final Bundle pSavedInstanceState) {
+		super.onCreate(pSavedInstanceState);
+
+		// Create the interstitial.
+		interstitial = new InterstitialAd(this);
+		interstitial.setAdUnitId(getResources().getString(
+				R.string.ad_unit_id_inter));
+
+		// Create ad request.
+		AdRequest adRequest = new AdRequest.Builder().build();
+
+		// Begin loading your interstitial.
+		interstitial.loadAd(adRequest);
+
+		interstitial.setAdListener(new AdListener() {
+			public void onAdLoaded() {
+				displayInterstitial();
+			}
+		});
+
+		// classic ads
+		mAdView = new AdView(this);
+		mAdView.setAdUnitId(getResources().getString(R.string.ad_unit_id));
+		mAdView.setAdSize(AdSize.SMART_BANNER);
+		RelativeLayout layout = (RelativeLayout) findViewById(R.id.myAds);
+		layout.addView(mAdView);
+		mAdView.loadAd(new AdRequest.Builder().build());
+	}
+
+	// Invoke displayInterstitial() when you are ready to display an
+	// interstitial.
+	public void displayInterstitial() {
+		if (interstitial.isLoaded()) {
+			interstitial.show();
 		}
 	}
 
 	@Override
+	public void onLoadComplete() {
+		/*
+		 * try { Map<String, Object> options = new HashMap<String, Object>();
+		 * options.put(OpenFeintSettings.SettingCloudStorageCompressionStrategy,
+		 * OpenFeintSettings.CloudStorageCompressionStrategyDefault); // use the
+		 * below line to set orientation
+		 * options.put(OpenFeintSettings.RequestedOrientation,
+		 * ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); OpenFeintSettings
+		 * settings = new OpenFeintSettings(getString(R.string.app_name),
+		 * getString(R.string.productKey), getString(R.string.productSecret),
+		 * getString(R.string.clientAppID), options);
+		 * 
+		 * OpenFeint.initialize(this, settings, new OpenFeintDelegate() { }); }
+		 * catch (Exception e) {
+		 * 
+		 * }
+		 */
+	}
+
+	@Override
 	public Engine onLoadEngine() {
-		return AdEnviroment.createEngine(ScreenOrientation.LANDSCAPE, true, false);
+		return AdEnviroment.createEngine(ScreenOrientation.LANDSCAPE, true,
+				false);
 	}
 
 	@Override
@@ -58,15 +104,17 @@ public class PlantsVsBugs extends AdGameActivity {
 
 	@Override
 	public Scene onLoadScene() {
-		SplashScene splashScene = new SplashScene(this.mEngine.getCamera(), GameData.getInstance().mSplash, 0f, 1f, 1f);
-        splashScene.registerUpdateHandler(new TimerHandler(7f, new ITimerCallback() {
-        	@Override
-        	public void onTimePassed(final TimerHandler pTimerHandler) {
-        		AdEnviroment.getInstance().setScene(new MainMenu());
-        	}
-        }));
+		SplashScene splashScene = new SplashScene(this.mEngine.getCamera(),
+				GameData.getInstance().mSplash, 0f, 1f, 1f);
+		splashScene.registerUpdateHandler(new TimerHandler(7f,
+				new ITimerCallback() {
+					@Override
+					public void onTimePassed(final TimerHandler pTimerHandler) {
+						AdEnviroment.getInstance().setScene(new MainMenu());
+					}
+				}));
 		return splashScene;
-        //return new Game();
+		// return new Game();
 	}
-	
+
 }
